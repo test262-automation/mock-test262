7d0a539718a384f7a80e2f3e1d0a4d8ac07b741d
Amal Hussein <amal@bocoup.com>
2018-07-02 23:52:30 -0400

    "[javascriptcore-debug-test262-automation] updated curation log with latest revision sha's from export

--- implementation-contributed/javascriptcore/262-fully-curated-vendor-modified/array-indexof.js
+++ implementation-contributed/javascriptcore/262-fully-curated-vendor-modified/array-indexof.js
@@ -36,3 +36,11 @@ function shouldBe(actual, expected)
 
 
 // Modify array-indexof.js in the vendor repo
+
+    /*
+    ********************************** test262-automation **********************************
+    Summary: Source material changed after curation & deletion of exported file.
+        File Status: Fully curated & deleted
+        Source Status: Modified since curation & deletion.
+        Below is the current and modified source which was exported on 1530589941430
+    */
