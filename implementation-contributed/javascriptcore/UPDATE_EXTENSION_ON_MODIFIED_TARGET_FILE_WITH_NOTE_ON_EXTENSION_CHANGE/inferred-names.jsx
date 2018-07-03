623ef543529ecee954687ebfe230329d4e579ee6
test262-automation <test262-automation@bocoup.com>
2018-07-03 12:32:46 -0400

    "[javascriptcore-debug-test262-automation] updated curation log with latest revision sha's from export

--- implementation-contributed/javascriptcore/UPDATE_EXTENSION_ON_MODIFIED_TARGET_FILE_WITH_NOTE_ON_EXTENSION_CHANGE/inferred-names.jsx
+++ implementation-contributed/javascriptcore/UPDATE_EXTENSION_ON_MODIFIED_TARGET_FILE_WITH_NOTE_ON_EXTENSION_CHANGE/inferred-names.jsx
@@ -142,3 +142,11 @@ assert( ({["__proto__"]: ()=>{}}).__proto__.name === "__proto__" );
 assert( ({__proto__(){}}).__proto__.name === "__proto__" );
 assert( ({*__proto__(){}}).__proto__.name === "__proto__" );
 assert( ({__proto__(){}}).__proto__.name === "__proto__" );
+
+    /*
+    ********************************** test262-automation **********************************
+    Summary: Source file renamed after partial curation & modification of exported file.
+        File Status: Partially curated & modified.
+        Source Status: Renamed since export.
+        This file name and location now matches the source which was exported on Tue Jul 03 2018 12:32:26 GMT-0400 (Eastern Daylight Time)
+    */
