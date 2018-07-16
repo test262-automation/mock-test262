2f4314b7b7660fa5a0f0d1bf09e4032f679a4d2f
test262-automation <test262-automation@bocoup.com>
2018-07-16 14:31:41 -0400

    [javascriptcore-debug-test262-automation] changes from https://github.com/test262-automation/mock-webkit.git at sha 1a4026b7 on Mon Jul 16 2018 14:31:31 GMT-0400 (Eastern Daylight Time)

--- implementation-contributed/javascriptcore/262-partially-curated-vendor-deleted/date-negative-zero.js
+++ implementation-contributed/javascriptcore/262-partially-curated-vendor-deleted/date-negative-zero.js
@@ -4,3 +4,11 @@ function shouldBe(actual, expected) {
 }
 
 // Partially curate date-negative-zero.js
+
+    /*
+    ********************************** test262-automation **********************************
+    Summary: Source file deleted after partial curation.
+        File Status: Partially curated & modified.
+        Source Status: Deleted since export.
+        This message was added on Mon Jul 16 2018 14:31:31 GMT-0400 (Eastern Daylight Time)
+    */
