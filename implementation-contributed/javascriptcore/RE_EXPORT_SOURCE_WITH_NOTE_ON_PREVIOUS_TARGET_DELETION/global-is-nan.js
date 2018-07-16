0717a40e8c8217ca244a460a10ed61435d660ee3
test262-automation <test262-automation@bocoup.com>
2018-07-16 09:01:09 -0400

    [javascriptcore-debug-test262-automation] updated curation log with latest revision sha's from export and changed files.     sourceRevisionAtLastExport: 1a4026b7 targetRevisionAtLastExport: 1418889

--- implementation-contributed/javascriptcore/RE_EXPORT_SOURCE_WITH_NOTE_ON_PREVIOUS_TARGET_DELETION/global-is-nan.js
+++ implementation-contributed/javascriptcore/RE_EXPORT_SOURCE_WITH_NOTE_ON_PREVIOUS_TARGET_DELETION/global-is-nan.js
@@ -19,3 +19,11 @@ shouldBe(isNaN("Hello"), true);
 shouldBe(isNaN("NaN"), true);
 
 // modify global-is-nan.js in the vendor repo
+
+    /*
+    ********************************** test262-automation **********************************
+    Summary: Source material changed after curation & deletion of exported file.
+        File Status: Fully curated & deleted
+        Source Status: Modified since curation & deletion.
+        Below is the current and modified source which was exported on Mon Jul 16 2018 08:59:45 GMT-0400 (Eastern Daylight Time)
+    */
