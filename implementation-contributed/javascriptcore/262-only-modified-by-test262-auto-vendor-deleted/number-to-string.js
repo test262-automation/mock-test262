a410ae5a1f29b6a1c6d1651a71f0206d1678bd37
test262-automation <test262-automation@bocoup.com>
2018-07-17 15:33:20 -0400

    [javascriptcore-debug-test262-automation] changes from https://github.com/test262-automation/mock-webkit.git at sha 1a4026b7 on Tue Jul 17 2018 15:32:56 GMT-0400 (Eastern Daylight Time)

--- implementation-contributed/javascriptcore/262-only-modified-by-test262-auto-vendor-deleted/number-to-string.js
+++ implementation-contributed/javascriptcore/262-only-modified-by-test262-auto-vendor-deleted/number-to-string.js
@@ -80,3 +80,11 @@ function expected(num, radix)
 }
 
 // modify number-to-string.js
+
+    /*
+    ********************************** test262-automation **********************************
+    Summary: Source file deleted after partial curation.
+        File Status: Partially curated & modified.
+        Source Status: Deleted since export.
+        This message was added on Tue Jul 17 2018 15:32:56 GMT-0400 (Eastern Daylight Time)
+    */
