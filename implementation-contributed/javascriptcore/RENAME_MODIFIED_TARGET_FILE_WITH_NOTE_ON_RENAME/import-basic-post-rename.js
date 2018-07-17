23dae958b148fa1133ea858de3b26e6a86bcd442
test262-automation <test262-automation@bocoup.com>
2018-07-17 15:38:45 -0400

    [javascriptcore-debug-test262-automation] changes from https://github.com/test262-automation/mock-webkit.git at sha 1a4026b7 on Tue Jul 17 2018 15:38:23 GMT-0400 (Eastern Daylight Time)

--- implementation-contributed/javascriptcore/RENAME_MODIFIED_TARGET_FILE_WITH_NOTE_ON_RENAME/import-basic-post-rename.js
+++ implementation-contributed/javascriptcore/RENAME_MODIFIED_TARGET_FILE_WITH_NOTE_ON_RENAME/import-basic-post-rename.js
@@ -41,3 +41,11 @@ var abort = $vm.abort;
     print(String(error));
     abort();
 });
+
+    /*
+    ********************************** test262-automation **********************************
+    Summary: Source file renamed after partial curation & modification of exported file.
+        File Status: Partially curated & modified.
+        Source Status: Renamed since export.
+        This file name and location now matches the source which was exported on Tue Jul 17 2018 15:38:23 GMT-0400 (Eastern Daylight Time)
+    */
