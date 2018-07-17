7f0d3850a4dce428fdebf29b3e0a8b486a38e367
test262-automation <test262-automation@bocoup.com>
2018-07-17 15:36:15 -0400

    [javascriptcore-debug-test262-automation] updated curation log with latest revision sha's from export and changed files.     sourceRevisionAtLastExport: 1a4026b7 targetRevisionAtLastExport: 1418889

--- implementation-contributed/javascriptcore/262-fully-curated-vendor-modified/array-indexof.js
+++ implementation-contributed/javascriptcore/262-fully-curated-vendor-modified/array-indexof.js
@@ -36,3 +36,11 @@ function shouldBe(actual, expected)
 
 
 // Modify array-indexof.js in the vendor repo
+
+    /*
+    ********************************** test262-automation **********************************
+    Summary: Source material changed after curation & deletion of exported file.
+        File Status: Fully curated & deleted
+        Source Status: Modified since curation & deletion.
+        Below is the current and modified source which was exported on Tue Jul 17 2018 15:35:52 GMT-0400 (Eastern Daylight Time)
+    */
