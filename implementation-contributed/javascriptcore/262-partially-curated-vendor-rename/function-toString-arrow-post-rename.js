0789d14d05e0ef2b342aef976456b1502589a834
test262-automation <test262-automation@bocoup.com>
2018-07-16 20:25:55 -0400

    [javascriptcore-debug-test262-automation] changes from https://github.com/test262-automation/mock-webkit.git at sha 1a4026b7 on Mon Jul 16 2018 20:08:37 GMT-0400 (EDT)

---
+++ implementation-contributed/javascriptcore/262-partially-curated-vendor-rename/function-toString-arrow-post-rename.js
@@ -0,0 +1,11 @@
+// Modify function-toString-arrow.js
+(async () => {}).toString();
+// Modify function-toString-arrow.js
+
+    /*
+    ********************************** test262-automation **********************************
+    Summary: Source file renamed after partial curation & modification of exported file.
+        File Status: Partially curated & modified.
+        Source Status: Renamed since export.
+        This file name and location now matches the source which was exported on Mon Jul 16 2018 20:08:37 GMT-0400 (EDT)
+    */
