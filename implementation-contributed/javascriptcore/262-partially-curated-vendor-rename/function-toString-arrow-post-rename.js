1c2ae208d70148e56d496dc3392f9679c432fcad
test262-automation <test262-automation@bocoup.com>
2018-08-03 11:45:43 -0400

    [javascriptcore-debug-test262-automation] updated curation log with latest revision sha's from export and changed files.     sourceRevisionAtLastExport: 1a4026b7 targetRevisionAtLastExport: 1418889

--- implementation-contributed/javascriptcore/262-partially-curated-vendor-rename/function-toString-arrow-post-rename.js
+++ implementation-contributed/javascriptcore/262-partially-curated-vendor-rename/function-toString-arrow-post-rename.js
@@ -1,3 +1,11 @@
 // Modify function-toString-arrow.js
 (async () => {}).toString();
 // Modify function-toString-arrow.js
+
+    /*
+    ********************************** test262-automation **********************************
+    Summary: Source file renamed after partial curation & modification of exported file.
+        File Status: Partially curated & modified.
+        Source Status: Renamed since export.
+        This file name and location now matches the source which was exported on Fri Aug 03 2018 11:45:26 GMT-0400 (Eastern Daylight Time)
+    */
